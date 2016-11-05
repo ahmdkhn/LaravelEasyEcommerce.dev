ac98c9716dfc837ddc8637036d9b542200a95220
AK <ahmdkhn@yahoo.com>
2016-11-05 20:47:38 +0100

    updates and experiments

---
+++ resources/views/about/index.blade.php
@@ -0,0 +1,7 @@
+@extends ('app')
+
+@section('content')
+<h3> This section is extended from index.php</h3>
+@stop
+
+
---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <head>
+ <meta charset="utf-8">
+<meta name="viewport" content="width=device-width,
+ initial-scale=1">
+ <link rel="stylesheet" type="text/css" href="/css/app.css">
+<title>Welcome Home</title>
+</head>
+<body>
+<h1>
+Main php file that yields the contents
+</h1>
+<div> 
+
+@yield ('content')
+
+</div>
+
+
+</body>
+ </html>
